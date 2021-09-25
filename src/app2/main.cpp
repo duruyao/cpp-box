959a745ecb4b350fa41d99d4884d5b1ab3950eee
duruyao <duruyao@gmail.com>
2021-09-25 10:38:26 +0800

    test(app): add some codes for testing

--- src/app2/main.cpp
+++ src/app2/main.cpp
@@ -1,9 +1,33 @@
 //
 // Created by dry on 7/1/21.
 //
+
 #include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <cJSON.h>
+#include <zmq.hpp>
+
+void sayHelloBycJSON() {
+    auto root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "word", cJSON_CreateString("hello, word"));
+    std::cout << cJSON_Print(root) << std::endl;
+    cJSON_Delete(root);
+}
+
+void runZmqServer() {
+    zmq::context_t ctx(1);
+    zmq::socket_t socket(ctx, ZMQ_REP);
+    socket.bind("tcp://*8081");
+    for (int n = 9; n--;) {
+        zmq::message_t req;
+        socket.recv(req, zmq::recv_flags::none);
+        std::cout << req.to_string() << std::endl;
+    }
+}
+
 
 int main(int argc, char **argv) {
-    fprintf(stdout, "hello world\n");
+    sayHelloBycJSON();
     return 0;
 }
