3b0b44893e070489c7b89a1f067679bbc71426eb
duruyao <duruyao@gmail.com>
2021-09-25 10:37:27 +0800

    build(cmake): rename some verables

--- tools/cmake/ProtoToCc.cmake
+++ tools/cmake/ProtoToCc.cmake
@@ -1,22 +1,23 @@
 if (ProtoBuf_FOUND)
-    if (NOT Proto_SOURCE_DIRS OR NOT EXISTS ${Proto_SOURCE_DIRS})
+    if (NOT Proto_SOURCE_DIR OR NOT EXISTS ${Proto_SOURCE_DIR})
         file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/proto)
-        set(Proto_SOURCE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/proto)
+        set(Proto_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/proto)
     endif ()
 
-    if (NOT Proto_DESTINATION_DIRS OR NOT EXISTS ${Proto_DESTINATION_DIRS})
+    if (NOT Proto_DESTINATION_DIR OR NOT EXISTS ${Proto_DESTINATION_DIR})
         file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/proto)
-        set(Proto_DESTINATION_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/proto)
+        set(Proto_DESTINATION_DIR ${CMAKE_CURRENT_SOURCE_DIR}/proto)
     endif ()
-
-    file(GLOB Proto_SOURCES CONFIGURE_DEPENDS ${Proto_SOURCE_DIRS}/*.proto)
-
-    add_custom_command(OUTPUT Proto_TO_CC_CMD
-            COMMAND ${ProtoBuf_EXECUTABLE} -I ${Proto_SOURCE_DIRS} --cpp_out=${Proto_DESTINATION_DIRS} ${Proto_SOURCES}
-            DEPENDS ${Proto_SOURCES})
 
     if (NOT Proto_TO_CC_TARGET)
         set(Proto_TO_CC_TARGET "proto2cc")
     endif ()
+
+    file(GLOB Proto_SOURCES CONFIGURE_DEPENDS ${Proto_SOURCE_DIR}/*.proto)
+
+    add_custom_command(OUTPUT Proto_TO_CC_CMD
+            COMMAND ${ProtoBuf_EXECUTABLE} -I ${Proto_SOURCE_DIR} --cpp_out=${Proto_DESTINATION_DIR} ${Proto_SOURCES}
+            DEPENDS ${Proto_SOURCES})
+
     add_custom_target(${Proto_TO_CC_TARGET} DEPENDS Proto_TO_CC_CMD)
 endif ()
