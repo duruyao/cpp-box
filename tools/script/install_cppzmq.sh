ec7a11bde2ffbc039feadb913cdc8e3fe256cc4f
duruyao <duruyao@gmail.com>
2022-06-06 17:42:46 +0800

    refactor(tools): refactor script `install_cppzmq.sh`

--- tools/script/install_cppzmq.sh
+++ tools/script/install_cppzmq.sh
@@ -1,43 +1,74 @@
 #! /bin/bash
 
-## date:    2021.06.21
-## file:    compile and install cppzmq
-## author:  duruyao@hikvision.com
+## date:    2021-05-17
+## author:  duruyao@gmail.com
+## desc:    download, build and install cppzmq
 ## release: https://github.com/zeromq/cppzmq/releases
 
-if [ $# != 3 ]; then
-  printf "USAGE (sudo permission may be needed):\n"
-  printf "    %s <ZIP_PATH> <INSTALL_PATH> <LIBZMQ_PATH>\n" "${0}"
+set -euo pipefail
+
+function error_ln() {
+  printf "\033[1;32;31m%s\n\033[m" "$1"
+}
+
+function warning_ln() {
+  printf "\033[1;33m%s\n\033[m" "$1"
+}
+
+function info_ln() {
+  printf "\033[0;32;32m%s\n\033[m" "$1"
+}
+
+function show_usage() {
+  cat <<EOF
+Usage: bash $0 <INSTALL_PATH> <LIBZMQ_INSTALL_PATH>
+
+Download, build and install ${package} (sudo permission maybe required)
+EOF
+}
+
+cores="$(($(nproc) / 2))"
+package="cppzmq"
+url="https://github.com/zeromq/cppzmq/archive/refs/tags/v4.8.1.tar.gz"
+
+if [ ${#} != 2 ]; then
+  show_usage >&2
   exit 1
 fi
 
-zip_path="${1}"
-ins_path="${2}"
-libzmq_path="${3}"
-new_folder="$(dirname "${zip_path}")/cppzmq_source"
+install_path="$1"
+libzmq_install_path="$2"
+mkdir -p "${install_path}"
 
-mkdir -p "${new_folder}" && rm -rf "${new_folder:?}/"*
-unzip -q "${zip_path}" -d "${new_folder}"
+download_path="/tmp/${package}-$(date | md5sum | head -c 6)"
+mkdir -p "${download_path}"
 
-src_path="${new_folder}/$(ls "${new_folder}")"
+cd "${download_path}"
+info_ln "Downloading ${package} from ${url} to ${download_path}"
+curl -LSso "${package}".tar.gz "${url}"
+tar -zxvf "${package}".tar.gz 1>/dev/null
 
-mkdir -p "${ins_path}" && rm -rf "${ins_path:?}/"*
+# shellcheck disable=SC2035
+source_path="${PWD}/$(ls -d */)"
+build_path="${source_path}/build"
+mkdir -p "${build_path}" && rm -rf "${build_path:?}/"*
 
-build_path=${src_path}/build
-
-mkdir -p "${build_path}" && cd "${build_path}" || exit
-
-cmake .. -DCPPZMQ_BUILD_TESTS=OFF \
-  -DCMAKE_INSTALL_PREFIX="${ins_path}" \
-  -DCMAKE_PREFIX_PATH="${libzmq_path}" \
-  -DCMAKE_CXX_FLAGS="-I${libzmq_path}/include" \
+cmake -S"${source_path}" -B"${build_path}" \
+  -DCPPZMQ_BUILD_TESTS=OFF \
+  -DCMAKE_INSTALL_PREFIX="${install_path}" \
+  -DCMAKE_PREFIX_PATH="${libzmq_install_path}" \
+  -DCMAKE_CXX_FLAGS="-I${libzmq_install_path}/include" \
   -DCMAKE_BUILD_WITH_INSTALL_RPATH=OFF \
-  -DCMAKE_INSTALL_RPATH="${ins_path}/lib" \
+  -DCMAKE_INSTALL_RPATH="${install_path}/lib" \
   -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON
 
-make -j16 install
+cmake --build "${build_path}" --target all --jobs="${cores}"
+cmake --build "${build_path}" --target install
 
-printf "\n"
-printf "Install cppzmq to \`${ins_path}\`:\n"
-ls -all "${ins_path}"
-printf "\n"
+echo ""
+info_ln "Install ${package} to ${install_path}"
+if [ -n "$(command -v tree)" ]; then
+  tree "${install_path}" -L 1
+else
+  ls -all "${install_path}"
+fi
