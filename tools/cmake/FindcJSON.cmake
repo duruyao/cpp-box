1fa586b4d4dfe0a8b2acbd05cf670e40dc66e64f
duruyao <duruyao@gmail.com>
2022-06-15 07:27:59 +0800

    refactor(tools): refactor cmake file `FindcJSON.cmake`

--- tools/cmake/FindcJSON.cmake
+++ tools/cmake/FindcJSON.cmake
@@ -1,13 +1,34 @@
+## date:   2022-06-14
+## author: duruyao@gmail.com
+## desc:   find cjson
+
+# find cjson
+#
+# find_package(cJSON [version] [EXACT] [QUIET] [MODULE]
+#         [REQUIRED] [[COMPONENTS] [components...]]
+#         [OPTIONAL_COMPONENTS components...]
+#         [REGISTRY_VIEW <64|32|64_32|32_64|HOST|TARGET|BOTH> ]
+#         [NO_POLICY_SCOPE]
+#         [GLOBAL])
 if (NOT cJSON_FOUND)
-    if (cJSON_HOME AND EXISTS ${cJSON_HOME})
-        find_path(cJSON_INCLUDE_DIRS NAMES cJSON.h PATHS ${cJSON_HOME} REQUIRED NO_DEFAULT_PATH)
-        find_path(cJSON_SOURCE_DIRS NAMES cJSON.c PATHS ${cJSON_HOME} REQUIRED NO_DEFAULT_PATH)
-    else ()
-        find_path(cJSON_HOME NAMES cJSON.h PATHS ${PROJECT_SOURCE_DIR}/third_party/cJSON REQUIRED NO_DEFAULT)
-        find_path(cJSON_INCLUDE_DIRS NAMES cJSON.h PATHS ${cJSON_HOME} REQUIRED NO_DEFAULT_PATH)
-        find_path(cJSON_SOURCE_DIRS NAMES cJSON.c PATHS ${cJSON_HOME} REQUIRED NO_DEFAULT_PATH)
-    endif ()
+    find_path(cJSON_ROOT
+            NAMES cJSON.h
+            PATHS ${CMAKE_SOURCE_DIR}/third_party/cJSON
+            REQUIRED
+            NO_DEFAULT)
+    find_path(cJSON_INCLUDE_DIRS
+            NAMES cJSON.h
+            PATHS ${cJSON_ROOT}
+            REQUIRED
+            NO_DEFAULT_PATH)
+    find_file(cJSON_SOURCES
+            NAMES cJSON.c
+            PATHS ${cJSON_ROOT}
+            REQUIRED
+            NO_DEFAULT_PATH)
 
     include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(cJSON FOUND_VAR cJSON_FOUND REQUIRED_VARS cJSON_HOME cJSON_INCLUDE_DIRS cJSON_SOURCE_DIRS)
+    find_package_handle_standard_args(cJSON
+            FOUND_VAR cJSON_FOUND
+            REQUIRED_VARS cJSON_INCLUDE_DIRS cJSON_SOURCES)
 endif ()
